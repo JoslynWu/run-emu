f5813646a9d883390fe1a9c731f1606999c9f8ec
Joslyn <cs_joslyn@foxmail.com>
2018-09-28 12:26:15 +0800

    update to 1.0.0

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const PlatformType = {
 
 // ---------- command ----------
 progrm
-    .version('0.2.2')
+    .version('1.0.0')
     .option('-l --list [ios|android]', 'list iOS simulator or android emulator.')
 
 progrm
